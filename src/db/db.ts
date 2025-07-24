47efdbffb390321199676baa0ed1e8dd3ff0af0f
the-name-romel <racopra@sorsu.edu.ph>
2025-07-24 22:53:18 +0800

    add drizzle config:

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "./schema";
+import * as schema from "@/db/schema";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
